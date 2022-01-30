a48e7494361b7dcfd5ec9647f6db27715c58c6cd
Samuel Dobbie <samuel.jordan.dobbie@gmail.com>
2022-01-30 21:53:01 +0000

    Update popup text & theme

--- src/commons/Theme.ts
+++ src/commons/Theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material"
 const theme = createTheme({
   palette: {
     primary: {
-      main: "rgb(93 123 255)",
+      main: "rgb(139 217 181)",
     },
     secondary: {
       main: "#eceded",
--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { render } from "react-dom"
-import { Button, Paper, Typography } from "@mui/material"
+import { Button, Divider, Paper, Typography } from "@mui/material"
 import { ThemeProvider } from "@emotion/react"
 import { theme } from "../commons/Theme"
 
@@ -18,32 +18,69 @@ function Popup(): JSX.Element {
       <Paper
         sx={{
           height: "100%",
-          textAlign: "center",
           padding: 2,
         }}
       >
         <Typography
           gutterBottom
-          variant="body2"
+          variant="h6"
           sx={{ opacity: 0.5 }}
         >
-          Periodically run a clean to move dead
-          bookmarks to a bookmark folder called
-          "Trash" for your review.
-
-          Occasionally run a refresh to update
-          the titles and favicons of your bookmarks.
-          During the refresh all of your bookmarks
-          will be automatically opened and closeed
-          (in small batches), so may take a couple
-          of minutes.
+          Clean Bookmarks
         </Typography>
 
-        <Button onClick={handleClean}>
+        <Typography
+          gutterBottom
+          variant="body2"
+          sx={{
+            opacity: 0.5,
+            fontSize: 12,
+            marginBottom: 2,
+          }}
+        >
+          Moves dead bookmarks (e.g. non-existant pages) to a folder called
+          "Trash" for you to review. No bookmarks will be deleted.
+        </Typography>
+
+        <Button onClick={handleClean} variant="contained" sx={{ color: "white" }} fullWidth>
           Clean bookmarks (Instant)
         </Button>
 
-        <Button onClick={handleRefresh}>
+        <Divider sx={{ margin: 2 }} />
+
+        <Typography
+          gutterBottom
+          variant="h6"
+          sx={{ opacity: 0.5 }}
+        >
+          Refresh Bookmarks
+        </Typography>
+
+        <Typography
+          gutterBottom
+          variant="body2"
+          sx={{
+            opacity: 0.5,
+            fontSize: 12,
+          }}
+        >
+          Updates bookmark titles and favicons. All pages will be
+          slowly opened and closed, so can take up to two minutes.
+        </Typography>
+
+        <Typography
+          gutterBottom
+          variant="body2"
+          sx={{
+            opacity: 0.5,
+            fontSize: 12,
+            marginBottom: 2,
+          }}
+        >
+          Please don't use the browser while the refresh is running.
+        </Typography>
+
+        <Button onClick={handleRefresh} variant="contained" sx={{ color: "white" }} fullWidth>
           Refresh bookmarks (Slow)
         </Button>
       </Paper>
